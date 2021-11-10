7ce04277697af51a7cae888f05512924ad5e8853
Joel Hans <joel.g.hans@gmail.com>
2021-11-09 17:48:29 -0700

    Fix prettier issues

--- scripts/generate-links.js
+++ scripts/generate-links.js
@@ -11,7 +11,7 @@ const BASE_CONTENT_PATH = './content'
   const files = glob.sync(`./content/articles/**/*.{md,mdx}`)
   if (!files.length) return false
 
-  let Links = new Array
+  let Links = new Array()
   await Promise.all(
     files.map(async (filepath) => {
       const source = await fsp.readFile(filepath)
@@ -25,22 +25,24 @@ const BASE_CONTENT_PATH = './content'
                   // _should_ indicate that we're working with an internal link
                   // between two posts.
                   if (node.tagName === 'a' && node.properties.href.startsWith('/')) {
-
-                    // Get the text of the parent element as best as possible, returning 
+                    // Get the text of the parent element as best as possible, returning
                     const text = parent.children.map((child) => {
                       let values
                       const safes = ['a', 'em', 'strong', 'code']
 
                       // Text children, which are the easiest to get the value from.
-                      if ( child.type === 'text' && child.value.length > 0 ) {
+                      if (child.type === 'text' && child.value.length > 0) {
                         values = child.value
                       }
-                      
+
                       // "safe" children, which reliably have a single `text`
                       // child of their own, which means we can easily grab the
                       // first item in the `children` array and pass along its
                       // `value`.
-                      else if ( safes.includes(child.tagName) && child.children[0].value.length > 0 ) {
+                      else if (
+                        safes.includes(child.tagName) &&
+                        child.children[0].value.length > 0
+                      ) {
                         values = child.children[0].value
                       }
 
@@ -59,7 +61,7 @@ const BASE_CONTENT_PATH = './content'
                       dest: node.properties.href,
                       src: filepath.replace(BASE_CONTENT_PATH, '').replace(/.(md|mdx)$/, ''),
                       title: matter(source).data.title,
-                      text: text.join('')
+                      text: text.join(''),
                     }
                     Links.push(link)
                   }
@@ -68,7 +70,7 @@ const BASE_CONTENT_PATH = './content'
             },
           ]
           return options
-        }
+        },
       })
     })
   )
