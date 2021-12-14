e4217fe0b2ef8c4e8aa5ff4a4bcd67044c74ae67
Joel Hans <joel.g.hans@gmail.com>
2021-12-14 14:24:34 -0700

    Fix 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,26 +1,29 @@
 import CustomLink from '@components/Link'
-import PageTitle from '@components/PageTitle'
+import Container from '@components/Container'
 
 export default function FourZeroFour() {
   return (
     <>
-      <header className="flex flex-row flex-wrap md:space-x-6 md:flex-nowrap mt-24">
-        <div>
-          <PageTitle>404</PageTitle>
+      <Container>
+        <header className="flex flex-row flex-wrap md:space-x-6 md:flex-nowrap mt-24">
+          <div>
+            <h1 className="text-orange text-3xl lg:text-6xl font-medium leading-snug mb-8">404</h1>
+          </div>
+        </header>
+        <div className="flex flex-row flex-nowrap items-start">
+          <div className="prose prose-md lg:prose-lg dark:prose-dark mb-24">
+            <p>Sorry, this page doesn't exist.</p>
+            <p>
+              Read about my <CustomLink href="/services">services</CustomLink>, learn a bit more
+              {` `}
+              <CustomLink href="/#about" className="hover:text-sea">
+                about me
+              </CustomLink>
+              , check out some of my past <CustomLink href="/#work">work</CustomLink>.
+            </p>
+          </div>
         </div>
-      </header>
-      <div className="flex flex-row flex-nowrap items-start">
-        <div className="prose prose-md lg:prose-lg dark:prose-dark mb-24">
-          <p>Sorry, this page doesn't exist.</p>
-          <p>
-            Read some of my <CustomLink href="/fiction">fiction</CustomLink>, learn a bit more{` `}
-            <CustomLink href="/about" className="hover:text-sea">
-              about me
-            </CustomLink>
-            , or find me on <CustomLink href="https://twitter.com/joelhans">Twitter</CustomLink>.
-          </p>
-        </div>
-      </div>
+      </Container>
     </>
   )
 }
