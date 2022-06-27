eafd8e5c609c9232033247be9b2f4c34b296cfc4
Joel Hans <j@joelhans.com>
2022-06-27 10:19:34 -0700

    Complete first pass of index rewrite

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,14 +1,9 @@
 import Image from 'next/image'
 
-const Container = ({ children, name, title, img }) => (
+export const Testimonial = ({ children, name, title, img }) => (
   <div className="flex flex-col items-center">
     <div className="col-span-2">
-      <Image
-        className="rounded-full"
-        src={`/static/testimonials/${img}`}
-        width={140}
-        height={140}
-      />
+      <Image className="rounded-lg" src={`/static/testimonials/${img}`} width={140} height={140} />
     </div>
     <div className="lg:w-2/3 prose dark:prose-dark text-center mt-4">
       <p className="text-sea text-2xl leading-snug">{children}</p>
@@ -19,4 +14,21 @@ const Container = ({ children, name, title, img }) => (
   </div>
 )
 
-export default Container
+export const TestimonialHoriz = ({ children, name, title, img }) => (
+  <div className="flex flex-wrap items-center">
+    <div className="flex flex-col justify-items-start items-start content-start lg:w-1/3">
+      <Image
+        className="grow-0 rounded-lg"
+        src={`/static/testimonials/${img}`}
+        width={128}
+        height={128}
+      />
+      <p className="font-mono text-lg font-bold text-center mt-4">
+        {name} • {title}
+      </p>
+    </div>
+    <div className="w-full lg:w-2/3 text-purple font-medium prose prose-lg dark:prose-dark">
+      <p>{children}</p>
+    </div>
+  </div>
+)
